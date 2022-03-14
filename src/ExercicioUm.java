ad559b848ad6907a70d88df141d5fc8dc0dd4446
Emilly-Vitoria <Emilly-Vitoria@test.com>
2022-03-14 10:51:35 -0300

    Exercícios de 14/03/2022

---
+++ src/ExercicioUm.java
@@ -0,0 +1,13 @@
+
+public class ExercicioUm {
+	
+public static void main(String[] args) {
+	String nome = "Horácio";
+	String sobrenome = "Augusto da Silveira";
+	int idade= 30;
+
+	
+	System.out.println(nome + " " + sobrenome + " tem "  + idade + " anos");
+	
+}
+}
---
+++ src/PrimeiroExemplo.java
@@ -0,0 +1,14 @@
+
+public class PrimeiroExemplo {
+
+	
+	public static void main(String[] args) {
+		
+		int idade = 16;
+		String nome= "Emilly";
+		System.out.println("Seu nome é: " + nome);
+		System.out.println("Sua idade é: " + idade);
+	
+		
+	}
+}
