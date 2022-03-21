3637b01d35d124736fe5fd1808a19ad399347aa5
EMilly-Vitoria <Emilly-Vitoria@test.com>
2022-03-21 10:41:01 -0300

    Desenvolvimento do exercicio 3

---
+++ src/ExercicioTres.java
@@ -0,0 +1,18 @@
+
+public class ExercicioTres {
+	
+	public static void main(String[] args) {
+		
+		int n1;
+		
+		if (n1> 0) {
+			System.out.println("1");
+		}
+		else if (n1 == 0) {
+			System.out.println("0");
+		}
+		else
+			System.out.println("-1");
+		 
+	}
+}
