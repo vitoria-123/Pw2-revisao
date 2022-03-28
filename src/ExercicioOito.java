c96a886c5eb57fd76bf668be6a9d75eef18747e8
emilly-vitoria <emilly-vitoria@test.com>
2022-03-28 10:40:20 -0300

    Desenvolvimento do exercicio 8

---
+++ src/ExercicioOito.java
@@ -0,0 +1,17 @@
+
+public class ExercicioOito {
+	
+	public static void main(String[] args) {
+		String diaSemana = "terca";
+
+		
+		if (diaSemana != "sabado" && diaSemana != "domingo") {
+			System.out.println("Pode ir ao banco.");
+		}
+		else {
+			System.out.println("Não pode ir ao banco, bom fds.");
+		}
+		
+	}
+
+}
