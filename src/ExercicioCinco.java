401dd2f0647f600c5b055b0ac57e2e9718ca9886
emilly-vitoria <emilly-vitoria@test.com>
2022-03-28 09:47:04 -0300

    Desenvolvimento do exercicio 5

--- src/ExercicioCinco.java
+++ src/ExercicioCinco.java
@@ -1,4 +1,17 @@
 
 public class ExercicioCinco {
 
-}
+	public static void main(String[] args) {
+		double trabalhoDeHistoria = 8.0;
+		double trabalhoDeMatematica = 7.0;
+		double trabalhoDeCiencia = 10;
+		double trabalhoDeGeografia = 9.5; 
+		double todasAsMaterias = trabalhoDeHistoria + trabalhoDeMatematica + trabalhoDeCiencia + trabalhoDeGeografia;
+		double media = 0;
+		
+		media = todasAsMaterias / 4;
+		
+		System.out.println("Media é :" + media);
+		
+	}
+	}
