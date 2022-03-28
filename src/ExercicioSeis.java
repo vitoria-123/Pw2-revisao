312e0c2a189660646b96183ffb67ea01115a8682
emilly-vitoria <emilly-vitoria@test.com>
2022-03-28 10:15:37 -0300

    Desenvolvimento do exercicio 6

---
+++ src/ExercicioSeis.java
@@ -0,0 +1,18 @@
+
+public class ExercicioSeis {
+
+	public static void main(String[] args) {
+		
+		double peso = 11;
+		
+		if (peso < 4) {
+			System.out.println("Abaixo do peso.");
+		}
+		else if (peso > 10) {
+			System.out.println("Acima do peso.");
+		}
+		else {
+			System.out.println("Peso normal.");
+		}
+	}
+}
